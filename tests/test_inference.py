02fd0ad8def9bb0368c6e413a3b3fbf6df373a3d
Gowtham Arepalli <gowthamarepalli09@gmail.com>
2025-08-04 14:07:13 +0530

    Refactor prediction function test to use consistent key naming for input parameters

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -6,12 +6,12 @@ def test_model_file_exists_and_not_empty():
 
 def test_prediction_function():
     sample_input = {
-        "type": "L",
-        "air_temperature": 298.1,
-        "process_temperature": 308.6,
-        "rotational_speed": 1551,
-        "torque": 42.8,
-        "tool_wear": 0
-    }
+    "Type": "L",
+    "Air temperature [K]": 298.1,
+    "Process temperature [K]": 308.6,
+    "Rotational speed [rpm]": 1551,
+    "Torque [Nm]": 42.8,
+    "Tool wear [min]": 0
+}
     result = predict(sample_input)
     assert result in [0, 1], "Prediction should be either 0 or 1"
