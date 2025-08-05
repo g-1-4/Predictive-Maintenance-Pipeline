9cc099a808187c4cfe72b76590a141063f41c828
Gowtham Arepalli <gowthamarepalli09@gmail.com>
2025-08-06 01:19:09 +0530

    Add CORS middleware to enable cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.schemas import predictionRequest
 from app.inference import predict
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Predictive Maintenance API", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
